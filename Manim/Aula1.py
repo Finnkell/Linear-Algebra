c47473f00728ae0405f41a6f0ab61b2b5f73161d
Eric Batista <klose.eric31@gmail.com>
2020-02-02 18:21:46 -0300

    Update Aula1.py

--- Manim/Aula1.py
+++ Manim/Aula1.py
@@ -9,7 +9,14 @@ class Animation(Scene):
         self.play(Write(Tex1))
 
         Tex2 = TextMobject("""
-        Se $W_1$, $W_2 \\subseteq{V}$ subespaços, já discutimos a soma $W = W_1 + W_2$, i.é, o subespaço de todos os $\\alpha = \\alpha_1 + \\alpha_2$ com $\\alpha_i \in W_i$. Uma situação particularmente agradável ocorre quando $W_1$ e $W_2$ são disjuntos. De fato, neste caso, um dado vetor $\\alpha \\in W$ pode ser escrito sob a forma $\\alpha = \\alpha_1 + \\alpha_2$, $\\alpha_i \\in W_i$, de uma única maneira.
+        \\begin{flushleft}
+        Se $W_1$, $W_2 \\subseteq{V}$ subespaços, já discutimos a soma $W = W_1 + W_2$, i.é, o subespaço de todos os $\\alpha = \\alpha_1 + \\alpha_2$ com $\\alpha_i \\in W_i$. Uma situação particularmente agradável ocorre quando $W_1$ e $W_2$ são disjuntos. De fato, neste caso, um dado vetor $\\alpha \\in W$ pode ser escrito sob a forma $\\alpha = \\alpha_1 + \\alpha_2$, $\\alpha_i \\in W_i$, de uma única maneira. Isto resulta do fato de que se também temos $\\alpha = \\beta_1 + \\beta_2$ com $\\beta_i \\in W_i$, então:
+
+        $$\\alpha_1 + \\alpha_2 = \\beta_1 + \\beta_2$$
+
+        de modo que
+
+        \\end{flushleft}
         """)
         Tex2.set_width(2*(FRAME_X_RADIUS-1))
         Tex2.next_to(Tex1, BOTTOM, aligned_edge=LEFT)
