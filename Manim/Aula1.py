a22b4beb0e8a7261acbd092e4e9a7a41afb6ff59
Eric Batista <klose.eric31@gmail.com>
2020-02-02 23:12:38 -0300

    Update Animation in Aula1.py

--- Manim/Aula1.py
+++ Manim/Aula1.py
@@ -23,7 +23,7 @@ class Animation(Scene):
 
         Tex3 = TextMobject("""
         \\begin{flushleft}
-        Como $\\alpha_1 - \\beta_1 \\in W_1$ e $\\beta_2 - \\alpha_2 \\in W_2$, devemos ter $\\alpha_1 - \\beta_1 = \\beta_2 - \\alpha_2 = 0$, i.é, $\\alpha_1 = \\beta_1$ e $\\alpha_2 = \\beta_2$. Quando $W_1$ e $W_2$ forem disjuntos diremos que a soma $W = W_1 + W_2$ é direta, ou que $W$ é \\textbf{soma direta} de $W_1$ e $W_2$.
+        Como $\\alpha_1 - \\beta_1 \\in W_1$ e $\\beta_2 - \\alpha_2 \\in W_2$, devemos ter $\\alpha_1 - \\beta_1 = \\beta_2 - \\alpha_2 = 0$, i.é, $\\alpha_1 = \\beta_1$ e $\\alpha_2 = \\beta_2$. Quando $W_1$ e $W_2$ forem disjuntos diremos que a soma $W = W_1 + W_2$ é direta, ou que $W$ é \\textbf{soma direta} de $W_1$ e $W_2$. A importância das somas diretas está no fato de que se $W = W_1 \\bigoplus W_2$, podemos estudar $W$ através dos pares de vetores $(\\alpha_1, \\alpha_2)$ com $\\alpha_i \\in W_i$.
 
         \\end{flushleft}
         """)
@@ -31,8 +31,10 @@ class Animation(Scene):
         Tex2.set_width(2*(FRAME_X_RADIUS-1))
         Tex2.next_to(Tex1, BOTTOM, aligned_edge=LEFT)
         self.play(Write(Tex2))
+        self.wait(3)
         self.clear()
         Tex3.set_width(2*(FRAME_X_RADIUS-1))
         Tex3.to_edge(UP)
         self.play(Transform(Tex2, Tex3))
+        self.wait(3)
 
